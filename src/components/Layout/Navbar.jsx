77597ba0814245d3a5b2530c1d8fc30a76a136c0
jaswanth107 <jaswanthitla@gmail.com>
2026-06-01 09:45:04 +0530

    import issue resolved

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -2,7 +2,7 @@
 
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-import "./Navbar.css";
+import "./navbar.css";
 
 const Navbar = () => {
   const { logout } = useAuth();
